d1fd790b6c3b52c220654bbfcbfc7237ce628612
Cláudio Sulo <109469430+SuloDS@users.noreply.github.com>
2024-02-22 14:55:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="Replicate Image Generator",
                    page_icon=":bridge_at_night:",
                    layout="wide")
 icon.show_icon(":foggy:")
-st.markdown("# :rainbow[Your Text-to-Image Artistry Studio]")
+st.markdown("# :rainbow[Conheca o seu estúdio artístico que transforma texto em imagem]")
 
 # API Tokens and endpoints from `.streamlit/secrets.toml` file
 REPLICATE_API_TOKEN = st.secrets["REPLICATE_API_TOKEN"]
@@ -109,9 +109,9 @@ def main_page(submitted: bool, width: int, height: int, num_outputs: int,
         negative_prompt (str): Text prompt for elements to avoid in the image.
     """
     if submitted:
-        with st.status('👩🏾‍🍳 Whipping up your words into art...', expanded=True) as status:
-            st.write("⚙️ Model initiated")
-            st.write("🙆‍♀️ Stand up and strecth in the meantime")
+        with st.status('👩🏾‍🍳 Transformando suas palavras em arte...', expanded=True) as status:
+            st.write("⚙️ Modelo iniciado")
+            st.write("🙆‍♀️ Enquanto isso, levante-se e alongue-se")
             try:
                 # Only call the API if the "Submit" button was pressed
                 if submitted:
@@ -135,7 +135,7 @@ def main_page(submitted: bool, width: int, height: int, num_outputs: int,
                         )
                         if output:
                             st.toast(
-                                'Your image has been generated!', icon='😍')
+                                'A sua imagem foi gerada!', icon='😍')
                             # Save generated image to session state
                             st.session_state.generated_image = output
 
@@ -169,7 +169,7 @@ def main_page(submitted: bool, width: int, height: int, num_outputs: int,
                         # Create a download button for the zip file
                         st.download_button(
                             ":red[**Download All Images**]", data=zip_io.getvalue(), file_name="output_files.zip", mime="application/zip", use_container_width=True)
-                status.update(label="✅ Images generated!",
+                status.update(label="✅ Imagens geradas!",
                               state="complete", expanded=False)
             except Exception as e:
                 print(e)
@@ -182,19 +182,19 @@ def main_page(submitted: bool, width: int, height: int, num_outputs: int,
     # Gallery display for inspo
     with gallery_placeholder.container():
         img = image_select(
-            label="Like what you see? Right-click and save! It's not stealing if we're sharing! 😉",
+            label="Gostou do que está vendo? Clique com o botão direito e salve! Não é roubar se estamos compartilhando! 😉",
             images=[
                 "gallery/farmer_sunset.png", "gallery/astro_on_unicorn.png",
                 "gallery/friends.png", "gallery/wizard.png", "gallery/puppy.png",
                 "gallery/cheetah.png", "gallery/viking.png",
             ],
-            captions=["A farmer tilling a farm with a tractor during sunset, cinematic, dramatic",
-                      "An astronaut riding a rainbow unicorn, cinematic, dramatic",
-                      "A group of friends laughing and dancing at a music festival, joyful atmosphere, 35mm film photography",
-                      "A wizard casting a spell, intense magical energy glowing from his hands, extremely detailed fantasy illustration",
-                      "A cute puppy playing in a field of flowers, shallow depth of field, Canon photography",
-                      "A cheetah mother nurses her cubs in the tall grass of the Serengeti. The early morning sun beams down through the grass. National Geographic photography by Frans Lanting",
-                      "A close-up portrait of a bearded viking warrior in a horned helmet. He stares intensely into the distance while holding a battle axe. Dramatic mood lighting, digital oil painting",
+            captions=["Um fazendeiro cultivando uma fazenda com um trator durante o pôr do sol, cinematográfico, dramático",
+                      "Um astronauta montando um unicórnio arco-íris, cinematográfico e dramático",
+                      "Um grupo de amigos rindo e dançando em um festival de música, atmosfera alegre, fotografia de filme 35mm",
+                      "Um mago lançando um feitiço, intensa energia mágica brilhando em suas mãos, ilustração de fantasia extremamente detalhada",
+                      "Um cachorrinho fofo brincando em um campo de flores, profundidade de campo rasa, fotografia Canon",
+                      "Uma mãe chita amamenta seus filhotes na grama alta do Serengeti. O sol da manhã brilha através da grama. Fotografia da National Geographic por Frans Lanting",
+                      "Aretrato de close-up de um guerreiro viking barbudo em um capacete com chifres. Ele olha intensamente para longe enquanto segura um machado de batalha. Iluminação ambiente dramática, pintura a óleo digital",
                       ],
             use_container_width=True
         )
